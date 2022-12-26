e67b18565175cce32d2cf08efe40840b15b0eb5c
Ramon Mesquita <34141267+nomar113@users.noreply.github.com>
2022-12-26 00:07:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// part 1
+
 var table = document.querySelector("#ctl50_g_cf05b8d5_fd75_46b5_bdfa_a623e654362c > div > div > table")
 
 var results = [];
@@ -25,4 +27,20 @@ while (table.querySelectorAll("tbody > tr")[i].innerText) {
 
   result = [];
   i = i + 1;
+}
+
+// part 2
+
+var resultsStringify = JSON.stringify(results);
+var firstGameStringify = JSON.stringify(['006', '015', '023', '031', '037', '059']);
+var myGameStringify = JSON.stringify(['002', '013', '019', '022', '040', '050']);
+
+var existFirstGame = resultsStringify.indexOf(firstGameStringify);
+if (existFirstGame != -1) {
+  console.log('First game present');
+}
+
+var existMyGame = resultsStringify.indexOf(myGameStringify);
+if (existMyGame != -1) {
+  console.log('My game present');
 }
